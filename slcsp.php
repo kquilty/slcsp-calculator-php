1acf5066a653cb9799b49e8fda4702ff6ea104a6
Kyle Quilty <kquilty@gmail.com>
2025-10-24 11:34:48 -0400

    - fetch silver plans and sort

--- slcsp.php
+++ slcsp.php
@@ -1,14 +1,30 @@
 <?php
 // Determine the "Second Lowest Cost Silver Plan" (SLCSP) for a list of ZIP codes.
-//
-//      Usage: php slcsp.php
-//
+// Usage:  php slcsp.php
 
+
+// First, let's just read everything in.
 $plan_rows  = readCsv('./input/plans[76].csv');
 $zip_rows   = readCsv('./input/zips[62].csv');
 $slcsp_rows = readCsv('./input/slcsp[20].csv');
 
-echo print_r($slcsp_rows, true);
+// Fetch only the SILVER plan rates, grouped as "state-rate_area"
+$rate_areas = [];
+foreach ($plan_rows as $cur_plan) {
+    if ($cur_plan['metal_level'] === 'Silver') {
+        $key = $cur_plan['state'] . '-' . $cur_plan['rate_area'];
+        $rate_areas[$key][] = floatval($cur_plan['rate']);
+    }
+}
+
+// Remove "ties" and sort
+foreach ($rate_areas as $key => &$rates) {
+    $rates = array_unique($rates);
+    sort($rates, SORT_NUMERIC);
+}
+unset($rates);
+
+echo print_r($rate_areas, true);
 
 
 function readCsv($file) {
